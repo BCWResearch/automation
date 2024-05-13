13a347777cd68884515aaf96d8cd3e04ab472064
vishal-bcw <166620914+vishal-bcw@users.noreply.github.com>
2024-05-13 10:38:36 -0400

    Add files via upload

---
+++ diskalert.sh
@@ -0,0 +1,14 @@
+ALERT=90 # alert level 
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read -r output;
+do
+  echo "$output"
+  usep=$(echo "$output" | awk '{ print $1}' | cut -d'%' -f1 )
+  partition=$(echo "$output" | awk '{ print $2 }' )
+  if [ $usep -ge $ALERT ]; then
+    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)"
+	sudo su sol
+	cp -p /dev/null ~/log/validator.log
+  fi
+done
+  
+  
